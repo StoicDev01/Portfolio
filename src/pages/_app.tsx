be8ea808df04da68fb24bde5af181dfa530f3193
izkeas <izkeasyomito@gmail.com>
2023-02-13 12:16:46 -0300

    First commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  palette : {
+    mode : "dark"
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
